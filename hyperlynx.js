1bece0db6e3c50a198200f0abc98c213daaebb06
Julien Wilhelm <contact@awebsome.fr>
2020-11-09 23:02:30 +0100

    Hyperlynx now compares the correct  domain names, even if there are subdomains.

--- hyperlynx.js
+++ hyperlynx.js
@@ -1,13 +1,32 @@
-let currentURL = new URL(window.location);
+// Extract domain name and its extension.
+getDomain = (url) => {
 
+	let host = new URL(url).host.split('.');	
+	console.log({
+		name: host[host.length - 2],
+		extension: host[host.length - 1]
+	});
+	
+	return {
+		name: host[host.length - 2],
+		extension: host[host.length - 1]
+	};
+
+};
+
+// Get reference.
+let visitedDomain = getDomain(window.location);
+
+// Browse and compare.
 linkElms = document.getElementsByTagName('a');
 
 for(let linkElm of linkElms){
-    
-    let newUrl = new URL(linkElm.href); 
-    
-    if(newUrl.host != currentURL.host) {
-        linkElm.classList.add('hyperlinx-warning');
-    }
+	
+	let proposedDomain = getDomain(linkElm.href);
+
+
+	if(proposedDomain.name != visitedDomain.name) {
+		linkElm.classList.add('hyperlinx-warning');
+	}
 
 }
