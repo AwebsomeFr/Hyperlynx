8e5622528f6d3c697f7d3369c54112a052ad1c7b
Julien Wilhelm <contact@awebsome.fr>
2020-11-10 23:41:05 +0100

    Refactoring codebase.

--- hyperlynx.js
+++ hyperlynx.js
@@ -1,40 +1,53 @@
-// Return a domain name and its extension.
+// Functions
 
-	getDomain = (url) => {
+	// Return a domain name and its extension from an URL.
 
-		let host = new URL(url).host.split('.');
-				
-		return {
-			name: host[host.length - 2],
-			extension: host[host.length - 1],
+		getDomain = (url) => {
+
+			let host = new URL(url).host.split('.');
+					
+			return {
+				name: host[host.length - 2],
+				extension: host[host.length - 1],
+			};
+
 		};
+		
+	// Compare two domains (name + extension).
 
-	};
+		areTheDomainsTheSame = (visitedDomain, proposedDomain) => {
 
-// Get the reference domain.
+			if(proposedDomain.name != undefined) { // Exclude #, javascript:, malformed url...
 
-	let visitedDomain = getDomain(window.location);
-
-// Browse and compare.
-
-	linkElms = document.getElementsByTagName('a');
-
-	for(let linkElm of linkElms){
-
-		let proposedDomain = getDomain(linkElm.href);
-
-		if(proposedDomain.name != undefined) { // Exclude #, javascript:, malformed url...
-
-			if(
-				(proposedDomain.name != visitedDomain.name) || // If different domain...
-				(proposedDomain.name == visitedDomain.name && proposedDomain.extension != visitedDomain.extension) // Or if same domain, but different extension...
-			) {
-				linkElm.classList.add('hyperlinx-warning');
+				if(
+					(proposedDomain.name != visitedDomain.name) ||
+					(proposedDomain.name == visitedDomain.name && proposedDomain.extension != visitedDomain.extension)
+				) {
+					return false;
+				}
+	
+			}
+			
+			return true;
+			
+		};
+		
+	// Script
+		
+		// Get the reference domain.
+		
+			let visitedDomain = getDomain(window.location);
+		
+		// Analyze starting content.
+		
+			linkElms = document.getElementsByTagName('a');
+			
+			for(let linkElm of linkElms){
+				
+				if(!areTheDomainsTheSame(visitedDomain, getDomain(linkElm.href))) {
+					linkElm.classList.add('hyperlynx-warning');
+				}
+				linkElm.classList.add('hyperlynx-checked');
 		
 			}
 
-			// Else : visited and proposed domains are identicals.
-
-		}
-
-	}
