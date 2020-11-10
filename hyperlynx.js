025304d51589c95c8e4896994a4ad91874255fdd
Julien Wilhelm <contact@awebsome.fr>
2020-11-10 22:42:43 +0100

    Exclude the analyse of malformed urls (or #, javascript:...) and remove a forgotten console.log().

--- hyperlynx.js
+++ hyperlynx.js
@@ -2,15 +2,11 @@
 
 	getDomain = (url) => {
 
-		let host = new URL(url).host.split('.');	
-		console.log({
-			name: host[host.length - 2],
-			extension: host[host.length - 1]
-		});
-		
+		let host = new URL(url).host.split('.');
+				
 		return {
 			name: host[host.length - 2],
-			extension: host[host.length - 1]
+			extension: host[host.length - 1],
 		};
 
 	};
@@ -24,14 +20,21 @@
 	linkElms = document.getElementsByTagName('a');
 
 	for(let linkElm of linkElms){
-		
+
 		let proposedDomain = getDomain(linkElm.href);
 
-		if(
-			(proposedDomain.name != visitedDomain.name) || // If different domain...
-			(proposedDomain.name == visitedDomain.name && proposedDomain.extension != visitedDomain.extension) // Or if same domain, but different extension...
-		) {
-			linkElm.classList.add('hyperlinx-warning');
+		if(proposedDomain.name != undefined) { // Exclude #, javascript:, malformed url...
+
+			if(
+				(proposedDomain.name != visitedDomain.name) || // If different domain...
+				(proposedDomain.name == visitedDomain.name && proposedDomain.extension != visitedDomain.extension) // Or if same domain, but different extension...
+			) {
+				linkElm.classList.add('hyperlinx-warning');
+		
+			}
+
+			// Else : visited and proposed domains are identicals.
+
 		}
 
 	}
