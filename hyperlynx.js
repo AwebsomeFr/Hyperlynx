c46d93031869bce3556ff793ac4a81bbf6da25e5
Julien Wilhelm <contact@awebsome.fr>
2020-11-10 08:24:51 +0100

    Hyperlynx is now checking extensions to improve the accuracy of its diagnosis.

--- hyperlynx.js
+++ hyperlynx.js
@@ -1,32 +1,37 @@
-// Extract domain name and its extension.
-getDomain = (url) => {
+// Return a domain name and its extension.
 
-	let host = new URL(url).host.split('.');	
-	console.log({
-		name: host[host.length - 2],
-		extension: host[host.length - 1]
-	});
-	
-	return {
-		name: host[host.length - 2],
-		extension: host[host.length - 1]
+	getDomain = (url) => {
+
+		let host = new URL(url).host.split('.');	
+		console.log({
+			name: host[host.length - 2],
+			extension: host[host.length - 1]
+		});
+		
+		return {
+			name: host[host.length - 2],
+			extension: host[host.length - 1]
+		};
+
 	};
 
-};
+// Get the reference domain.
 
-// Get reference.
-let visitedDomain = getDomain(window.location);
+	let visitedDomain = getDomain(window.location);
 
 // Browse and compare.
-linkElms = document.getElementsByTagName('a');
 
-for(let linkElm of linkElms){
-	
-	let proposedDomain = getDomain(linkElm.href);
+	linkElms = document.getElementsByTagName('a');
 
+	for(let linkElm of linkElms){
+		
+		let proposedDomain = getDomain(linkElm.href);
 
-	if(proposedDomain.name != visitedDomain.name) {
-		linkElm.classList.add('hyperlinx-warning');
+		if(
+			(proposedDomain.name != visitedDomain.name) || // If different domain...
+			(proposedDomain.name == visitedDomain.name && proposedDomain.extension != visitedDomain.extension) // Or if same domain, but different extension...
+		) {
+			linkElm.classList.add('hyperlinx-warning');
+		}
+
 	}
-
-}
